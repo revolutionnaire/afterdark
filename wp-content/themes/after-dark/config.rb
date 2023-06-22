ecee5d7bdfa45da2686b8658a25681101c6389de
Samuel Bacay <revolutionnaire@users.noreply.github.com>
2023-06-22 16:42:44 +0800

    Use Compass and Sass

---
+++ wp-content/themes/after-dark/config.rb
@@ -0,0 +1,12 @@
+http_path = "/" # Root level target path
+css_dir = "." # Targets our default style.css file at the root level of our theme
+sass_dir = "assets/sass" # Targets our sass directory
+images_dir = "assets/images" # Targets our pre existing image directory
+javascripts_dir = "assets/js" # Targets our JavaScript directory
+
+# Set the output style to compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions.
+# Note: this is important in WordPress themes for sprites
+relative_assets = true
