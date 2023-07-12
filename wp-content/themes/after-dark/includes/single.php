96840356cedab5f6321fe0e4f544a23960230e6b
Samuel Bacay <revolutionnaire@users.noreply.github.com>
2023-07-12 09:02:19 +0800

    Add Mastodon share link

--- wp-content/themes/after-dark/includes/single.php
+++ wp-content/themes/after-dark/includes/single.php
@@ -15,11 +15,13 @@ function ad_social_media_links( $post ) {
   // Generate the social media sharing links
   $facebook_link = 'https://www.facebook.com/sharer/sharer.php?u=' . $post_url;
   $twitter_link = 'https://twitter.com/intent/tweet?url=' . $post_url;
+  $mastodon_link = 'https://mastodonshare.com/?url=' . $post_url;
 
   // Output the social media sharing links
   echo '<ul class="social-media-links list-links">';
   echo '<li><small>Share on</small></li>';
   echo '<li><a href="' . esc_url( $facebook_link ) . '" target="_blank" rel="nofollow">Facebook</a></li>';
   echo '<li><a href="' . esc_url( $twitter_link ) . '" target="_blank" rel="nofollow">Twitter</a></li>';
+  echo '<li><a href="' . esc_url( $mastodon_link ) . '" target="_blank" rel="nofollow">Mastodon</a></li>';
   echo '</ul>';
 }
