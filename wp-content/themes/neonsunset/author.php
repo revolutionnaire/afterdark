fa7f3efd976a1277f05551c711fa184700ec6b33
Samuel Bacay <revolutionnaire@users.noreply.github.com>
2025-01-29 06:43:45 +0800

    Refactor and clean up

--- wp-content/themes/neonsunset/author.php
+++ wp-content/themes/neonsunset/author.php
@@ -9,29 +9,27 @@
 
 get_header();
 ?>
-	<main class="wrapper">
-		<div class="content-card-wrapper">
-		<?php if ( have_posts() ) : the_post(); ?>
-			<div class="contributor-card text-center">
-			<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
-				<h1><?php  the_author_meta( 'display_name' ); ?></h1>
-			<?php if ( get_the_author_meta( 'description' ) !== '' ) : ?>
-				<p><?php esc_html( the_author_meta( 'description' ) ); ?></p>
-			<?php endif; ?>
-			<?php if ( get_the_author_meta( 'user_url' ) !== '' ) : ?>
-				<a class="button button-foreground" href="<?php esc_url( the_author_meta( 'user_url' ) ); ?>">Website &#8250;</a>
-			<?php endif; ?>
-			</div>
-		<?php rewind_posts(); while ( have_posts() ) : the_post();
-			if ( $post->post_type === 'guide' )
-				get_template_part( 'parts/content-card', 'content-card' );
-			endwhile;
-		else :
-		?>
-			<article class="content-card-message">
-				<h2>Sorry, no posts were found!</h2>
-			</article>
+	<main class="wrapper content-card-wrapper">
+	<?php if ( have_posts() ) : the_post(); ?>
+		<div class="contributor-card text-center">
+		<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
+			<h1><?php  the_author_meta( 'display_name' ); ?></h1>
+		<?php if ( get_the_author_meta( 'description' ) !== '' ) : ?>
+			<p><?php esc_html( the_author_meta( 'description' ) ); ?></p>
+		<?php endif; ?>
+		<?php if ( get_the_author_meta( 'user_url' ) !== '' ) : ?>
+			<a class="button button-foreground" href="<?php esc_url( the_author_meta( 'user_url' ) ); ?>">Website &#8250;</a>
 		<?php endif; ?>
 		</div>
+	<?php rewind_posts(); while ( have_posts() ) : the_post();
+		if ( $post->post_type === 'guide' )
+			get_template_part( 'parts/content-card', 'content-card' );
+		endwhile;
+	else :
+	?>
+		<article class="content-card-message">
+			<h2>Sorry, no posts were found!</h2>
+		</article>
+	<?php endif; ?>
 	</main>
 <?php get_footer(); ?>
--- wp-content/themes/neonsunset/index.php
+++ wp-content/themes/neonsunset/index.php
@@ -10,20 +10,18 @@
 get_header();
 ( is_category() ) ? ns_categories_as_breadcrumbs( $cat, true ) : null;
 ?>
-	<main class="wrapper">
-		<div class="content-card-wrapper">
-		<?php
-		if ( have_posts() ) :
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'parts/content-card', 'content-card' );
-			endwhile;
-		else :
-			?>
-			<article class="content-card-message">
-				<h2>Sorry, no posts were found!</h2>
-			</article>
-		<?php endif; ?>
-		</div>
+	<main class="wrapper content-card-wrapper">
+	<?php
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+			get_template_part( 'parts/content-card', 'content-card' );
+		endwhile;
+	else :
+		?>
+		<article class="content-card-message">
+			<h2>Sorry, no posts were found!</h2>
+		</article>
+	<?php endif; ?>
 	</main>
 <?php get_footer(); ?>
