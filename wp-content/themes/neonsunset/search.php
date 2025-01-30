2b1e7c7413fc38d86645bdfa484f479eef6a189a
Samuel Bacay <revolutionnaire@users.noreply.github.com>
2025-01-30 08:29:54 +0800

    Refactor class and ID of search bar

--- wp-content/themes/neonsunset/search.php
+++ wp-content/themes/neonsunset/search.php
@@ -9,7 +9,7 @@
 
 get_header();
 ?>
-	<form id="search-bar" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+	<form id="search" class="search-bar" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
 		<div class="wrapper">
 			<input type="search" class="input-text" name="s" placeholder="<?php echo ( ! empty( get_search_query() ) ? get_search_query() : 'Search for a location' ); ?>" autocomplete="off">
 		</div>
